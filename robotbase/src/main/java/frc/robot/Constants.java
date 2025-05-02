3fb2be84f87f3cee35767eba3df775a14b2f1632
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-05-01 19:54:27 -0500

    halfway through swerve implementation, Ive already got the swerve, and am currently in the process of creating the drivercontrols and the defaultdrive command

---
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class CONTROLLER {
+        public static final int DRIVE_CONTROLLER_PORT = 1;
+        public static final double DRIVE_CONTROLLER_DEADBAND = 0.01;
+        public static final int CODRIVER_CONTROLLER_PORT = 0;
+        public static final double CODRIVE_CONTROLLER_DEADBAND = 0.025;
+        public static final double DRIVE_RUMBLE_INTENSITY = .5;
+        public static final double CODRIVE_RUMBLE_INTENSITY = .5;
+        public static final double DRIVE_RUMBLE_SECONDS = 2;
+        public static final double CODRIVE_RUMBLE_SECONDS = 2;
+    }
+}
