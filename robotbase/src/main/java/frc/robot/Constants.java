d1c6f6cc36b82b72f7664f91298975734ebf9b63
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-05-13 18:30:29 -0500

    updated driver controls, added more constants, and mostly worked on the commandSwerveDrivetrain subsystem alongside the DefaultDrive command

--- robotbase/src/main/java/frc/robot/Constants.java
+++ robotbase/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import frc.robot.generated.TunerConstants;
+
 public final class Constants {
     public static final class CONTROLLER {
         public static final int DRIVE_CONTROLLER_PORT = 1;
@@ -10,5 +16,13 @@ public final class Constants {
         public static final double CODRIVE_RUMBLE_INTENSITY = .5;
         public static final double DRIVE_RUMBLE_SECONDS = 2;
         public static final double CODRIVE_RUMBLE_SECONDS = 2;
+    }
+
+    public static final class SWERVE {
+        public static final AngularVelocity MAX_ANGULAR_RATE = RotationsPerSecond.of(0.75);
+        public static final LinearVelocity MAX_SPEED = TunerConstants.kSpeedAt12Volts; // kSpeedAt12Volts
+        // desired top speed
+        // rotation per
+        // second
     }
 }
--- robotbase/src/main/java/frc/robot/commands/drive/DefaultDrive.java
+++ robotbase/src/main/java/frc/robot/commands/drive/DefaultDrive.java
@@ -1,11 +1,31 @@
 package frc.robot.commands.drive;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.Robot;
+import frc.robot.generated.TunerConstants;
 
 public class DefaultDrive extends Command {
     public DefaultDrive() {
         addRequirements(Robot.swerve);
     }
 
+    @Override
+    public void execute() {
+        double x = Robot.driverControls.getLeftX();
+        double y = Robot.driverControls.getLeftX();
+        double rotation = Robot.driverControls.getRightX();
+
+        if (x == 0 && y == 0 && rotation == 0) {
+            Robot.swerve.stopMotors();
+        } else {
+            Robot.swerve.driveFieldRelative(
+                    y * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond),
+                    x * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond),
+                    rotation * Constants.SWERVE.MAX_ANGULAR_RATE.in(RotationsPerSecond));
+        }
+    }
 }
