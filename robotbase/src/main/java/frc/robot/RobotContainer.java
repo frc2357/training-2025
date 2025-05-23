31a0f35c6860120b21830ac28287b9e50dad1191
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-05-22 19:47:10 -0500

    one more prettify

--- robotbase/src/main/java/frc/robot/RobotContainer.java
+++ robotbase/src/main/java/frc/robot/RobotContainer.java
@@ -10,25 +10,33 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.generated.TunerConstants;
 
 public class RobotContainer {
+
   private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
-                                                                                    // max angular velocity
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(
+    RadiansPerSecond
+  ); // 3/4 of a rotation per second
+  // max angular velocity
 
   /* Setting up bindings for necessary control of the swerve drive platform */
-  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+  private final SwerveRequest.FieldCentric drive =
+    new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1)
+      .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
       .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  private final SwerveRequest.SwerveDriveBrake brake =
+    new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point =
+    new SwerveRequest.PointWheelsAt();
 
-  private final Telemetry logger = new Telemetry(Constants.SWERVE.MAX_SPEED.in(Units.MetersPerSecond));
+  private final Telemetry logger = new Telemetry(
+    Constants.SWERVE.MAX_SPEED.in(Units.MetersPerSecond)
+  );
 
   public RobotContainer() {
     Robot.swerve.registerTelemetry(logger::telemeterize);
