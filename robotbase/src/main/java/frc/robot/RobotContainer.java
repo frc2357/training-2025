56d2cea05a34b6d8576e4b8179d0d9c1987ad0f3
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-05-22 19:41:11 -0500

    prettified once again

--- robotbase/src/main/java/frc/robot/RobotContainer.java
+++ robotbase/src/main/java/frc/robot/RobotContainer.java
@@ -18,8 +18,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController = new CommandXboxController(
-      OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController =
+    new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -32,7 +32,8 @@ public class RobotContainer {
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition).onTrue(
-        new ExampleCommand(m_exampleSubsystem));
+      new ExampleCommand(m_exampleSubsystem)
+    );
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is
     // pressed,
